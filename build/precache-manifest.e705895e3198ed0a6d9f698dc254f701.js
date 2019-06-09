fb981b81faf4633488ee6d136f36d0a36f29a796
Adam Kao <adamkao@gmail.com>
2019-06-09 15:19:27 -0700

    initial commit

---
+++ build/precache-manifest.e705895e3198ed0a6d9f698dc254f701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5cd723d44e98fc7400177e5cfe292e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "760a4019f385803461e0",
+    "url": "/static/css/main.a57e540e.chunk.css"
+  },
+  {
+    "revision": "97d5febff13796f13d87",
+    "url": "/static/js/2.c7156e8d.chunk.js"
+  },
+  {
+    "revision": "760a4019f385803461e0",
+    "url": "/static/js/main.d4b4979c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
