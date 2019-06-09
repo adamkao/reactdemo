0426abf2d6179c3a4b4b6dc85098ec1f23138680
adamkaodvc <44104211+adamkaodvc@users.noreply.github.com>
2019-06-08 18:01:35 -0700

    initial

---
+++ src/Sortable.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Sortable extends Component {
+	render () {
+		return (
+			<p>0 1 2 3 4 5 6 7 8 9</p>
+		);
+	}
+}
+
+export default Sortable;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Login from './Login';
+import Sortable from './Sortable';
+import Tabs from './Tabs';
+require('./styles.css');
+
+export
+function App() {
+	return (
+		<div>
+			<Login></Login>
+			<Tabs>
+				<div label="Setup 1">
+					<Sortable></Sortable>
+            		<p><button>Submit 1</button></p>
+				</div>
+				<div label="Setup 2">
+					<Sortable></Sortable>
+            		<p><button>Submit 2</button></p>
+				</div>
+				<div label="Setup 3">
+					<Sortable></Sortable>
+            		<p><button>Submit 3</button></p>
+				</div>
+				<div label="Setup 4">
+					<Sortable></Sortable>
+            		<p><button>Submit 4</button></p>
+				</div>
+				<div label="Setup 5">
+					<Sortable></Sortable>
+            		<p><button>Submit 5</button></p>
+				</div>
+				<div label="Setup 6">
+					<Sortable></Sortable>
+            		<p><button>Submit 6</button></p>
+				</div>
+			</Tabs>
+		</div>
+	);
+}
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+render(<App />, container);
